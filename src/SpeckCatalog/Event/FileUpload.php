2d0cd425129d70426e229f8e925f8fbbb8ebded9
Dranzd Viper <dranzd@gmail.com>
2013-08-27 19:35:16 -0700

    Replace variable name from  to

--- src/SpeckCatalog/Event/FileUpload.php
+++ src/SpeckCatalog/Event/FileUpload.php
@@ -6,12 +6,12 @@ class FileUpload
 {
     public function preFileUpload($e)
     {
-        $sm = $e->getTarget()->getServiceLocator();
+        $sl = $e->getTarget()->getServiceLocator();
 
         $formData = $e->getParam('params');
         $getter = 'get' . ucfirst($formData['file_type']) . 'Upload';
 
-        $catalogOptions = $sm->get('speckcatalog_module_options');
+        $catalogOptions = $sl->get('speckcatalog_module_options');
 
         if($formData['file_type'] === 'productDocument'){
             $e->getParam('options')->setAllowedFileTypes(array('pdf' => 'pdf'));
@@ -26,25 +26,25 @@ class FileUpload
 
     public function postFileUpload($e)
     {
-        $sm = $e->getTarget()->getServiceLocator();
+        $sl = $e->getTarget()->getServiceLocator();
         $params = $e->getParams();
         switch ($params['params']['file_type']) {
             case 'productImage' :
-                $imageService = $sm->get('speckcatalog_product_image_service');
+                $imageService = $sl->get('speckcatalog_product_image_service');
                 $image = $imageService->getModel();
                 $image->setProductId($params['params']['product_id'])
                     ->setFileName($params['fileName']);
                 $imageService->insert($image);
                 break;
             case 'productDocument' :
-                $documentService = $sm->get('speckcatalog_document_service');
+                $documentService = $sl->get('speckcatalog_document_service');
                 $document = $documentService->getEntityMapper();
                 $document->setProductId($params['params']['product_id'])
                     ->setFileName($params['fileName']);
                 $documentService->insert($document);
                 break;
             case 'optionImage' :
-                $imageService = $sm->get('speckcatalog_option_image_service');
+                $imageService = $sl->get('speckcatalog_option_image_service');
                 $image = $imageService->getEntityMapper();
                 $image->setOptionId($params['params']['option_id'])
                     ->setFileName($params['fileName']);
